82529aecaa9cfca7dbb78a8b90167ead959483b3
Sammie <samcoh@umich.edu>
2020-02-24 22:55:26 -0500

    changes

--- js/video.js
+++ js/video.js
@@ -1,52 +1,79 @@
-var video;
 
-function getVid(){
-	video = document.querySelector("#myVideo"); 
+var video = document.getElementById("myVideo");
+var speed = 1.0;
+
+
+function getVid() {
+  document.querySelector("#myVideo");
+
+}
+function playVid() {
+  console.log("Play Video");
+  video.play();
+  document.getElementById("volume").innerHTML = video.volume * 100 + "%";
 }
 
-function playVid() { 
 
-	console.log("Play Video");
-} 
+function pauseVid() {
+  console.log("Pause Video");
+  video.pause();
 
-function pauseVid() { 
+}
 
-	console.log("Pause Video");
-} 
-
-function decreaseSpeed() { 
-
-  	console.log("Speed is "+ );
-} 
+function decreaseSpeed() {
+  speed = speed - (speed * 0.2);
+  video.playbackRate = speed;
+  console.log("Speed is "+ speed);
+}
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
-} 
+  speed = speed * 1.25;
+  video.playbackRate = speed;
+	console.log("Speed is "+ speed);
+}
 
 function skipAhead() {
+  var skip = video.currentTime + 60;
 
-	console.log("Current location is "+ );
-} 
+  if (skip > video.duration){
+    video.currentTime = 0;
+    video.playbackRate = 1.0;
 
-function mute() { 
-  	
-  		console.log("Unmuted");
-  	
-      	console.log("Muted");
+  } else{
+    video.currentTime = skip;
+  }
+  console.log("Current location is "+ video.currentTime);
+
+}
+
+function mute() {
+  if (video.muted) {
+    video.muted = false;
+    document.getElementById("mute").innerHTML = "mute";
+    console.log("Unmuted");
+
+  } else{
+    video.muted = true;
+    document.getElementById("mute").innerHTML = "unmute";
+    console.log("Muted");
+  }
 }
 
 function changeVolume() {
-;	console.log("Volume is ");
+  var slider  = document.querySelector("#volumeSlider");
+  video.volume = slider.value / 100;
+  document.getElementById("volume").innerHTML = slider.value + "%";
+  console.log("Volume is "+ video.volume *100);
+
 }
-       
 
-function gray() { 
 
-	console.log("In grayscale")
+function gray() {
+  video.className = "grayscale";
+	console.log("In grayscale");
 }
 
 function color() {
-
-	console.log("In color") 
+  video.className = "video";
+	console.log("In color");
 }
